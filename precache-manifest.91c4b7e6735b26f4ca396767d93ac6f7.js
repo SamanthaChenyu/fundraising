d7fd4497148f2e05af4140de665652cc596b6857
SamanthaChenyu <same0730@gmail.com>
2021-06-30 11:58:06 +0800

    Updates

---
+++ precache-manifest.91c4b7e6735b26f4ca396767d93ac6f7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c3e086aa24d3b8bd1b9ce000ad944b3",
+    "url": "/fundraising/index.html"
+  },
+  {
+    "revision": "2214b4ff2dfe40dca76c",
+    "url": "/fundraising/static/css/2.270ac04a.chunk.css"
+  },
+  {
+    "revision": "7b0daf7bfcaaa9de1871",
+    "url": "/fundraising/static/css/main.093712c9.chunk.css"
+  },
+  {
+    "revision": "2214b4ff2dfe40dca76c",
+    "url": "/fundraising/static/js/2.836250f6.chunk.js"
+  },
+  {
+    "revision": "7b0daf7bfcaaa9de1871",
+    "url": "/fundraising/static/js/main.645edf6d.chunk.js"
+  },
+  {
+    "revision": "cad4620a1ea75e2dab0f",
+    "url": "/fundraising/static/js/runtime~main.ba8d27ca.js"
+  },
+  {
+    "revision": "111a72c17621ff088e5eabfb7dd1b89a",
+    "url": "/fundraising/static/media/share_cash.111a72c1.png"
+  },
+  {
+    "revision": "7f77adfcad60c5a4feb8e93c78f54217",
+    "url": "/fundraising/static/media/text.7f77adfc.png"
+  },
+  {
+    "revision": "00ffa4849a9274aedea09784a18e28d0",
+    "url": "/fundraising/static/media/text2.00ffa484.png"
+  }
+]);
